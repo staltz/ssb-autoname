de5541f9461c4baa3400157c548edd501c09f1e9
Jan Bölsche <regular.gonzales@googlemail.com>
2019-01-06 13:38:38 +0100

    Add tests for config.autoname

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
-var test = require('tape');
-var pull = require('pull-stream');
-var ssbKeys = require('ssb-keys');
-var {createSbot} = require('scuttlebot');
-const rimraf = require('rimraf')
+const test = require('tape');
+const pull = require('pull-stream');
+const ssbKeys = require('ssb-keys');
+const {createSbot} = require('scuttlebot');
+const rimraf = require('rimraf');
 
-var lucyKeys = ssbKeys.generate();
+const lucyKeys = ssbKeys.generate();
 
 test('sets the name to be the hostname', function(t) {
   process.env.HOST = 'wintermute'
-  var myTestSbot = createSbot().use(require('.'))({
+  const myTestSbot = createSbot().use(require('.'))({
     temp: true,
     keys: lucyKeys
   });
-  var lucy = myTestSbot.createFeed(lucyKeys);
 
   setTimeout(() => {
     pull(
@@ -62,7 +61,7 @@ test('pre-existing about message', function(t) {
     t.error(err)
 
     process.env.HOST = 'wintermute'
-    var myTestSbot = createSbot().use(require('.'))({
+    const myTestSbot = createSbot().use(require('.'))({
       path,
       keys: lucyKeys
     });
@@ -89,4 +88,66 @@ test('pre-existing about message', function(t) {
       );
     }, 300);
   });
+});
+
+test('env.HOST wins over config.autoname', function(t) {
+  process.env.HOST = 'wintermute'
+  const myTestSbot = createSbot().use(require('.'))({
+    temp: true,
+    keys: lucyKeys,
+    autoname: 'Lucy'
+  });
+
+  setTimeout(() => {
+    pull(
+      myTestSbot.createFeedStream({}),
+      pull.collect((err, msgs) => {
+        t.error(err);
+        t.equals(msgs.length, 1, 'only one message');
+        const msg = msgs[0];
+        t.equals(typeof msg.key, 'string', 'message has key');
+        t.equals(typeof msg.value, 'object', 'message has value');
+        t.equals(msg.value.author, lucyKeys.id, 'message author is lucy');
+        t.equals(typeof msg.value.content, 'object', 'value has content');
+        t.equals(msg.value.content.type, 'about', 'content type is about');
+        t.equals(
+          msg.value.content.name,
+          'wintermute',
+          'assigned name is correct'
+        );
+        myTestSbot.close( ()=> t.end() );
+      })
+    );
+  }, 300);
+});
+
+test('If env.HOST is unset, config.autoname is used', function(t) {
+  delete process.env.HOST
+  const myTestSbot = createSbot().use(require('.'))({
+    temp: true,
+    keys: lucyKeys,
+    autoname: 'Lucy'
+  });
+
+  setTimeout(() => {
+    pull(
+      myTestSbot.createFeedStream({}),
+      pull.collect((err, msgs) => {
+        t.error(err);
+        t.equals(msgs.length, 1, 'only one message');
+        const msg = msgs[0];
+        t.equals(typeof msg.key, 'string', 'message has key');
+        t.equals(typeof msg.value, 'object', 'message has value');
+        t.equals(msg.value.author, lucyKeys.id, 'message author is lucy');
+        t.equals(typeof msg.value.content, 'object', 'value has content');
+        t.equals(msg.value.content.type, 'about', 'content type is about');
+        t.equals(
+          msg.value.content.name,
+          'Lucy',
+          'assigned name is correct'
+        );
+        myTestSbot.close( ()=> t.end() );
+      })
+    );
+  }, 300);
 });
